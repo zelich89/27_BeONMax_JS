74f561dda2b74c1451073b651fad5f8ca732057f
Alexander <zelich89@gmail.com>
2020-06-21 20:48:22 +0300

    second les

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var first = 1;
-let second = 2;
-const c = 3;
+let money = prompt('Ваш бюджет на месяц?', '3000'),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '2020-06-21');
 
-console.log(second );
+let appDate = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeDate: time,
+    savings: false
+};
 
-let person = {
-    name: "Sasha",
-    age: 30,
-    isMarries: false
+for (let i = 0; i < 3; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', '1'),
+        b = prompt('Во сколько обойдется?', '2')
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        // сonsole.log('done');    
+        console.log("done");
+        appDate.expenses[a] = b;
+    }
+
+    appDate.moneyPerDay = appDate.budget / 30;
+
 }
 
-console.log(person[0]);
+alert("Ежежеднеыный бюджет :" + appDate.moneyPerDay);
+
+// console.log(appDate);
+
+if (appDate.moneyPerDay < 50) {
+    console.log("Минимальный");
+} else if (appDate.moneyPerDay > 50 && appDate.moneyPerDay < 1500) {
+    console.log('Средне');
+} else if (appDate.moneyPerDay > 1500) {
+    console.log('Много');
+} else {
+    console.log('Ошибка');
+
+}
+        
